d8647c0d0f68132d820a08f018604d1dc373b1eb
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-08-05 04:30:40 +0600

    add include/config.sample.php

---
+++ include/config.sample.php
@@ -0,0 +1,16 @@
+<?php
+
+// Database configuration
+define('DB_HOST', 'localhost'); // Database host
+
+define('DB_NAME', 'currency_converter'); // Database name
+
+define('DB_USER', 'root'); // Change if necessary
+
+define('DB_PASS', ''); // Change if necessary
+define('DB_CHARSET', 'utf8mb4'); // Change if necessary
+
+// API configuration
+define('API_URL', 'https://api.exchangerate-api.com/v4/latest/');
+define('API_KEY', 'your_api_key_here');
+?>
