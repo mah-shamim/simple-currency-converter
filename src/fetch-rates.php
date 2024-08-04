083f6ae130c851584f0f9bc6a22b66ad1f9e3723
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-08-05 04:35:10 +0600

    update src/fetch-rates.php

--- src/fetch-rates.php
+++ src/fetch-rates.php
@@ -2,7 +2,7 @@
 require '../include/db.php';
 
 // Fetch exchange rates from the external API
-$apiUrl = "https://api.exchangerate-api.com/v4/latest/USD"; // Replace with your API endpoint
+$apiUrl = API_URL . "USD"; // Replace with your API endpoint
 
 $response = file_get_contents($apiUrl);
 $data = json_decode($response, true);
