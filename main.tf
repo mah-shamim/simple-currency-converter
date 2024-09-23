0074a5c156743f1d508285999206199bf91ebf41
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-09-23 14:09:42 +0600

    Create a Terraform configuration file (`main.tf`)

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "docker" {}
+
+resource "docker_image" "simple_calculator" {
+  name         = "simple_calculator"
+  build {
+    context = "."
+  }
+}
+
+resource "docker_container" "app_container" {
+  image = docker_image.simple_calculator.name
+  name  = "simple_calculator_container"
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
