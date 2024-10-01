d6b5b2772bc710657e8e70bc79a0e00293335617
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-10-01 07:11:19 +0600

    Update full_deploy.sh

--- full_deploy.sh
+++ full_deploy.sh
@@ -22,5 +22,7 @@ docker run -d -p $PORT:80 --name $CONTAINER_NAME $DOCKER_IMAGE_TAG
 # Deploy to Kubernetes
 echo "Deploying to Kubernetes..."
 whoami
+su -
+whoami
 kubectl apply -f k8s-deployment.yml --validate=false
 kubectl apply -f k8s-service.yml --validate=false
